6e68a657e5a607acf5f4dc1ff6cea1163059a3ca
Fredd57s <150307936+Fredd57s@users.noreply.github.com>
2024-07-24 10:35:26 -0500

    Add files via upload

---
+++ Grafo_JimenezFreddy/src/main/java/com/mycompany/grafo_jimenezfreddy/Grafo_JimenezFreddy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grafo_jimenezfreddy;
+
+/**
+ *
+ * @author USER
+ */
+public class Grafo_JimenezFreddy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
