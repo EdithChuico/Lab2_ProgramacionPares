044e785626f18caba3c0e764b4593d3979ccec1c
EdithChuico <150299765+EdithChuico@users.noreply.github.com>
2024-07-24 10:32:40 -0500

    Add files via upload

---
+++ lab_Pares/src/main/java/com/mycompany/lab_pares/Lab_Pares.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_pares;
+
+/**
+ *
+ * @author ESPE
+ */
+public class Lab_Pares {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
